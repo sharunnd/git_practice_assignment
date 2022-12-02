f72935fad1df926d9efb84725c89fc0aa7c17501
bhabesh.bharali <jsarma058@gmail.com>
2022-12-02 22:45:56 +0530

    complete_work

--- prime_problem.js
+++ prime_problem.js
@@ -1,7 +1,14 @@
 let num=13;
 let count=0;
-for(i=0;i<=num;i++){
+for(let i=1;i<=num;i++){
 if(num%i==0){
   count++;
 }
-  }
+}
+
+if(count==2){
+console.log("it's a prime")
+}
+else{
+console.log("Not a prime number");
+}
